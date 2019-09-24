05cfd373f00fd4591b3241cbc18a97c99178d315
Ilya Osipov <070809010@mail.ru>
2019-09-24 19:11:39 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,21 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(
+    cap1_title="Красная кепка",
+    cap1_text="$ 100.00",
+    cap2_title="Чёрная кепка",
+    cap2_text="$ 120.00",
+    cap3_title="Ещё одна чёрная кепка",
+    cap3_text="$ 90.00",
+)
+
+with open('index.html', 'w') as file:
+    file.write(rendered_page)
